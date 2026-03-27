764d54655ffd48c8896abe6e9ba2991c0c30b0e6
joellarramendia <joellarramendia@gmail.com>
2026-03-26 21:50:36 -0300

    feat: the endpoint was created to retrieve the project by ID, to update it, and to delete it

--- server/src/controllers/ProjectController.ts
+++ server/src/controllers/ProjectController.ts
@@ -5,7 +5,7 @@ export class ProjectController {
 
     static createProject = async (req: Request, res: Response) => {
         const project = new Project(req.body)
-        
+
         try {
             await project.save()
             res.send('Proyecto creado correctamente')
@@ -14,7 +14,60 @@ export class ProjectController {
         }
     }
 
+
     static getAllProjects = async (req: Request, res: Response) => {
-        res.send('Todos los proyectos')
+        try {
+            const projects = await Project.find({})
+            res.json(projects)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    static getProjectById = async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            const project = await Project.findById(id)
+            if (!project) {
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({ error: error.message })
+            }
+            res.json(project)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    static updateProject = async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            const project = await Project.findByIdAndUpdate(id, req.body)
+            if (!project) {
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({ error: error.message })
+            }
+            await project.save()
+            res.send('Proyecto actualizado')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    static deleteProject = async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            const project = await Project.findById(id)
+            if (!project) {
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({ error: error.message })
+            }
+            await project.deleteOne()
+            res.send('Proyecto eliminado')
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
