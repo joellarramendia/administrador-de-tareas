fa05310d90c1c497041423d9d66013f1df892324
joellarramendia <joellarramendia@gmail.com>
2026-03-30 10:49:31 -0300

    feat: add project creation view and form with validation using react-hook-form

---
+++ client/src/views/projects/CreateProjectView.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import ProjectForm from "@/components/projects/ProjectForm"
+
+
+export default function CreateProjectView() {
+
+    const initialValues = {
+        projectName: '',
+        clientName: '',
+        description: ''
+    }
+    const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+    const handleForm = (data) => {
+        console.log(data)
+    }
+    return (
+        <>
+            <div className="max-w-3xl mx-auto">
+                <h1 className="text-5xl font-black">Crear Proyectos</h1>
+                <p className="text-2xl font-light text-gray-500 mt-5">Llena el siguiente formulario parra crear un proyecto</p>
+
+                <nav className="my-5">
+                    <Link
+                        to={'/'}
+                        className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+                    >
+                        Volver Proyectos
+                    </Link>
+                </nav>
+
+                <form
+                    className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+                    onSubmit={handleSubmit(handleForm)}
+                    noValidate
+                >
+
+                    <ProjectForm
+                    register={register}
+                    errors={errors}
+                    />
+
+                    <input
+                        type="submit"
+                        value='Crear Proyecto'
+                        className=" bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                    />
+
+                </form>
+            </div>
+        </>
+    )
+}
