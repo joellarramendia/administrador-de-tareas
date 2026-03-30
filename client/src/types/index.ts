4362829f855021fe82730821ee73b306546750d0
joellarramendia <joellarramendia@gmail.com>
2026-03-30 16:43:46 -0300

    feat: enhance DashboardView with project listing and options menu

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,12 +1,23 @@
-import {z} from 'zod'
+import { z } from 'zod'
 
 // PROJECTS
 export const projectSchema = z.object({
     _id: z.string(),
-    projectName:z.string(),
+    projectName: z.string(),
     clientName: z.string(),
     description: z.string()
 })
+
+
+export const dashboardProjectSchema = z.array(
+    projectSchema.pick({
+        _id: true,
+        projectName: true,
+        clientName: true,
+        description: true
+    })
+)
+
 export type Project = z.infer<typeof projectSchema>
 
 export type ProjectFormData = Pick<Project, 'projectName' | 'clientName' | 'description'>
