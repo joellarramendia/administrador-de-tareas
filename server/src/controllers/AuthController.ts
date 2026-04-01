f86f39af64c797715ecdbb8c90f0756a1a15432d
joellarramendia <joellarramendia@gmail.com>
2026-04-01 17:20:02 -0300

    feat: implement email confirmation for user registration with token generation

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,26 +1,62 @@
 import { Request, Response } from "express"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
+import Token from "../models/Token"
+import { generateToken } from "../utils/token"
+import { AuthEmail } from "../emails/AuthEmail"
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
         try {
-            const {password, email} = req.body
+            const { password, email } = req.body
 
             // Prevenir duplicados
-            const userExists = await User.findOne({email})
-            if(userExists){
+            const userExists = await User.findOne({ email })
+            if (userExists) {
                 const error = new Error('El usuario ya esta registrado')
-                return res.status(409).json({error: error.message})
+                return res.status(409).json({ error: error.message })
             }
             // Crea un usuario
             const user = new User(req.body)
             // Hash Password
             user.password = await hashPassword(password)
-            await user.save()
+            // Generar el token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user._id
+
+            // Enviar email
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.email,
+                token: token.token
+            })
+
+            await Promise.allSettled([user.save(), token.save()])
             res.send('Cuenta creada, revisa tu email para confirmarla')
         } catch (error) {
-            res.status(500).json({error: 'Hubo un error'})
+            res.status(500).json({ error: 'Hubo un error' })
+        }
+    }
+
+
+
+    static confirmAccount = async (req: Request, res: Response) => {
+        try {
+            const { token } = req.body
+            const tokenExists = await Token.findOne({ token })
+            if (!tokenExists) {
+                const error = new Error('Token no valido')
+                return res.status(401).json({ error: error.message })
+            }
+
+            const user = await User.findById(tokenExists.user)
+            user.confirmed = true
+
+            await Promise.allSettled([user.save(), tokenExists.deleteOne()])
+            res.send('Cuenta confirmada correctamente')
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' })
         }
     }
 }
