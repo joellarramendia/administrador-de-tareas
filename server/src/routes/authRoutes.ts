1fa4c1b2331de1c215dd1320796a65340629912e
joellarramendia <joellarramendia@gmail.com>
2026-04-01 16:11:55 -0300

    feat: implement user registration with password hashing and validation

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('desde auth')
-})
+router.post('/create-account',
+    body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('password')
+        .isLength({ min: 8 }).withMessage('La contraseña es muy corto, minimo 8 caracteres'),
+    body('password_confirmation').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Las contraseñas no son iguales')
+        }
+        return true
+    }),
+    body('email')
+        .isEmail().withMessage('E-mail no valido'),
+    handleInputErrors,
+    AuthController.createAccount
+)
 
 export default router
