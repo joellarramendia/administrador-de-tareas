85230dbbabe833c9840159f1eccc34cc923bc5db
joellarramendia <joellarramendia@gmail.com>
2026-04-04 23:24:19 -0300

    feat(auth): add token validation and password update functionality

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { AuthController } from "../controllers/AuthController";
 import { handleInputErrors } from "../middleware/validation";
 
@@ -54,6 +54,30 @@ router.post('/forgot-password',
         .isEmail().withMessage('E-mail no valido'),
     handleInputErrors,
     AuthController.forgotPassword
+)
+
+
+router.post('/validate-token',
+    body('token')
+        .notEmpty().withMessage('El token no puede ir vacio'),
+    handleInputErrors,
+    AuthController.validateToken
+)
+
+
+router.post('/update-password/:token',
+    param('token')
+        .isNumeric().withMessage('Token no válido'),
+    body('password')
+        .isLength({ min: 8 }).withMessage('La contraseña es muy corto, minimo 8 caracteres'),
+    body('password_confirmation').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Las contraseñas no son iguales')
+        }
+        return true
+    }),
+    handleInputErrors,
+    AuthController.updatePasswordWithToken
 )
 
 
