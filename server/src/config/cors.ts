27fcc11bd4d7dcc967230d27c6056397c1a2a228
joellarramendia <joellarramendia@gmail.com>
2026-04-10 21:47:19 -0300

    fix

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,17 +1,39 @@
 import { CorsOptions } from "cors"
 
+// export const corsConfig: CorsOptions = {
+//     origin: function (origin, callback) {
+//         const whitelist = [process.env.FRONTEND_URL]
+
+//         if(process.argv[2] == '--api'){
+//             whitelist.push(undefined)
+//         }
+
+//         if (whitelist.includes(origin)) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Error de CORS'))
+//         }
+//     }
+// } 
+
+
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
         const whitelist = [process.env.FRONTEND_URL]
 
-        if(process.argv[2] == '--api'){
-            whitelist.push(undefined)
+        // En desarrollo, permitimos localhost
+        if (process.env.NODE_ENV !== 'production') {
+            whitelist.push('http://localhost:5173')
         }
 
-        if (whitelist.includes(origin)) {
+        // 1. !origin permite herramientas como Postman o el flag --api
+        // 2. whitelist.includes(origin) permite tu frontend
+        if (!origin || whitelist.includes(origin)) {
             callback(null, true)
         } else {
+            // Esto te ayudará a ver en los Logs de Render quién intenta entrar
+            console.log("CORS rechazado desde el origen:", origin)
             callback(new Error('Error de CORS'))
         }
     }
-} 
+}
