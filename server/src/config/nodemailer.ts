85d4cf8e28133a7bb3439acc193dbabb8d2a98fe
joellarramendia <joellarramendia@gmail.com>
2026-07-20 16:52:12 -0300

    fix: change nodemailer configuration to disable secure connection and add TLS options

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -6,10 +6,14 @@ const config = () => {
     return {
         host: process.env.SMTP_HOST,
         port: +process.env.SMTP_PORT,
-        secure: true,
+        secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
+        },
+        tls: {
+            // Esto le dice a Nodemailer que exija una conexión cifrada segura
+            rejectUnauthorized: false
         },
         // Fuerza a Node.js a preferir IPv4 sobre IPv6 para evitar el ENETUNREACH
         dnsLookup: (hostname, options, callback) => {
