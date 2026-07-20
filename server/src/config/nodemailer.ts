079fcaf912f3ef2a4a39cbbfed02da09339ca599
joellarramendia <joellarramendia@gmail.com>
2026-07-20 17:14:25 -0300

    refactor: comment out unused nodemailer configuration options

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -5,20 +5,20 @@ dotenv.config()
 const config = () => {
     return {
         host: process.env.SMTP_HOST,
-        port: +process.env.SMTP_PORT,
-        secure: false,
+        // port: +process.env.SMTP_PORT,
+        // secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
         },
-        tls: {
-            // Esto le dice a Nodemailer que exija una conexión cifrada segura
-            rejectUnauthorized: false
-        },
-        // Fuerza a Node.js a preferir IPv4 sobre IPv6 para evitar el ENETUNREACH
-        dnsLookup: (hostname, options, callback) => {
-            require('dns').lookup(hostname, { family: 4 }, callback);
-        }
+        // tls: {
+        //     // Esto le dice a Nodemailer que exija una conexión cifrada segura
+        //     rejectUnauthorized: false
+        // },
+        // // Fuerza a Node.js a preferir IPv4 sobre IPv6 para evitar el ENETUNREACH
+        // dnsLookup: (hostname, options, callback) => {
+        //     require('dns').lookup(hostname, { family: 4 }, callback);
+        // }
     }
 }
 
